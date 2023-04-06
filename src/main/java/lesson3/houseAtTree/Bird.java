b30617ae6298c3005db4aedd27cbfd737aa4f7d5
dianatikhomirova <dianagrigoreva94@gmail.com>
2023-04-06 09:09:26 +0200

    Delete not required files

--- src/main/java/lesson3/houseAtTree/Bird.java
+++
@@ -1,4 +0,0 @@
-package lesson3.houseAtTree;
-
-public class Bird extends Animal implements Flyable {
-}
--- src/main/java/lesson4/hw3/Apple.java
+++
@@ -1,7 +0,0 @@
-package lesson4.hw3;
-
-public class Apple extends Fruit {
-    public Apple(float weight) {
-        super(1);
-    }
-}
