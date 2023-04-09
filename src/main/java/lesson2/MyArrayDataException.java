9114922dbc87a65f40547cd44ea787c9ccda8c1d
dianatikhomirova <dianagrigoreva94@gmail.com>
2023-04-09 23:15:27 +0200

    Done

---
+++ src/main/java/lesson2/MyArrayDataException.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+public class MyArrayDataException extends RuntimeException {
+    public int i;
+    public int j;
+
+    MyArrayDataException(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+}
